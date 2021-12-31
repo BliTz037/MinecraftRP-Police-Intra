5c8d0281df6844d1bf7104152d543ebf62389d64
BliTz <60670940+BliTz037@users.noreply.github.com>
2021-12-31 21:51:18 +0100

    [ADD] Middleware auth / authAdmin

---
+++ back-end/src/middleware/auth.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from 'jsonwebtoken';
+import User from "../db/models/User";
+
+const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.headers.authorization)
+        return res.status(403).json({response: "No token, authorization denied"});
+    const token = req.headers.authorization.split(' ')[1];
+    verify(token, process.env.JSON_WEB_TOKEN || "", async (err, authData) => {
+        if (err)
+            return res.status(403).json({ response: "Wrong token !"});
+        console.log(authData);
+        let existUser = await User.findOne({ where: { id: authData!.id  , username: authData!.username } });
+        if (!existUser)
+            return res.status(403).json({ response: "Token expired !"});
+        next();
+    });
+}
+
+const checkAuthAdmin = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.headers.authorization)
+        return res.status(403).json({response: "No token, authorization denied"});
+    const token = req.headers.authorization.split(' ')[1];
+    verify(token, process.env.JSON_WEB_TOKEN || "", async (err, authData) => {
+        if (err)
+            return res.status(403).json({ response: "Wrong token !"});
+        console.log(authData);
+        let existUser = await User.findOne({ where: { id: authData!.id  , username: authData!.username, isAdmin: true } });
+        if (!existUser)
+            return res.status(403).json({ response: "Token expired or you don't have permission to do this !"});
+        next();
+    });
+}
+
+export {
+    checkAuth,
+    checkAuthAdmin
+};
--- back-end/src/routes/auth.ts
+++ back-end/src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { register, login } from "../controllers/auth";
+import { checkAuthAdmin } from "../middleware/auth";
 
 const AuthRouter:Router = Router();
 
-AuthRouter.post('/register', register);
+AuthRouter.post('/register', checkAuthAdmin, register);
 AuthRouter.post('/login', login);
 
 export default AuthRouter;
