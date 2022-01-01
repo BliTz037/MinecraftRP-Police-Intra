5096f2fc7746be000958109105880409984e0ded
BliTz <60670940+BliTz037@users.noreply.github.com>
2022-01-01 14:04:36 +0100

    [ADD] Initialization table crime

--- back-end/src/db/init.ts
+++ back-end/src/db/init.ts
@@ -1,35 +1,37 @@
 import User from "./models/User";
 import Criminal from "./models/Criminal";
 import * as bcrypt from "bcrypt";
+import Crime from "./models/Crime";
 
 const isDev = process.env.NODE_ENV === 'development'
 
 const adminInit = async () => {
-    User.findOne({ where: { username: "admin"}}).then(async (result) => {
+    User.findOne({ where: { username: "admin" } }).then(async (result) => {
         if (result) {
             console.log("Administrator account OK");
             return;
         }
         console.log("No administrator account created. Creation in progress");
-        let passwordHash:string = await bcrypt.hash(process.env.ADMIN_PASSWORD || "admin", 10);
+        let passwordHash: string = await bcrypt.hash(process.env.ADMIN_PASSWORD || "admin", 10);
         User.create({
             username: "admin",
             usernameGame: "God",
-            password:  passwordHash,
+            password: passwordHash,
             rank: "Administrator",
             registrationNumber: 9999999,
             isAdmin: true
         }).then(() => {
             console.log("Administrator created !");
             console.log("Username:", "admin");
-            console.log("Password:", process.env.ADMIN_PASSWORD|| "admin");
-        }).catch((error) => {console.error(error)});
+            console.log("Password:", process.env.ADMIN_PASSWORD || "admin");
+        }).catch((error) => { console.error(error) });
     })
 }
 
 const dbInit = async () => {
     await User.sync({ alter: isDev });
     await Criminal.sync({ alter: isDev });
+    await Crime.sync({ alter: isDev });
     await adminInit();
 };
 
